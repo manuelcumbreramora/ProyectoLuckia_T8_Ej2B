9b26a22b6270503a0d284a113dcaf4aa7794e19a
jesus.changbuccheri <jesus.changbuccheri@PLX300000000664>
2020-04-15 13:59:31 +0200

    WIP: Presentación Admin

---
+++ Ejercicio_T8_2B/PresentacionAdmin.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NegocioCasino;
+
+namespace Ejercicio_T8_2B {
+    public partial class PresentacionAdmin : System.Web.UI.Page {
+        Dictionary<string, bool> Juegos = new Dictionary<string, bool>();
+        protected void Page_Load(object sender, EventArgs e) {
+            //Obtener datos relevantes
+            var nombres = RepositoryJuego.ObtenerJuegos(false);
+            var estados = RepositoryJuego.ObtenerEstadosJuegos(false);
+            //Llenar ListaJuegos con todos los juegos en base de datos
+            if (ListaJuegos.Items.Count == 0)
+            foreach (string s in nombres)
+            {
+                ListaJuegos.Items.Add(s);
+            }
+            //Crear diccionario con nombre:estado
+            for (int i = 0; i < nombres.Count; i++ )
+            {
+                Juegos.Add(nombres[i], estados[i]);
+            }
+            Update_ActiveCheckbox();
+
+        }
+
+        protected void Update_ActiveCheckbox() 
+        {
+            CheckBoxActivo.Checked = Juegos[ListaJuegos.SelectedItem.Text];
+        }
+
+        protected void ListaJuegos_SelectedIndexChanged(object sender, EventArgs e) {
+            Update_ActiveCheckbox();
+        }
+    }
+}
--- NegocioCasino/RepositoryJuego.cs
+++ NegocioCasino/RepositoryJuego.cs
@@ -9,14 +9,28 @@ namespace NegocioCasino {
     public static class RepositoryJuego {
         public static List<String> ObtenerJuegos(bool activo, float minBet = 0) {
             var dao = new DAOBImplJuego();
-            
-
             var list = new List<String>();
+
             foreach (DTOJuego o in dao.listarJuegos(activo, minBet) )
             {
                 list.Add( o.nombre );
             }
             return list;
+        }
+
+        public static List<bool> ObtenerEstadosJuegos(bool activo, float minBet = 0) {
+            var dao = new DAOBImplJuego();
+            var list = new List<bool>();
+
+            foreach (DTOJuego o in dao.listarJuegos(activo, minBet))
+            {
+                list.Add(o.activo);
+            }
+            return list;
+        }
+
+        public static void CambiarEstadoJuego() {
+
         }
     }
 }
